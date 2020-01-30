1477a45b282e46cf73b0059355497a3f83213300
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 19:51:10 +0530

    Create sonarAssociateProject.groovy

---
+++ vars/sonarAssociateProject.groovy
@@ -0,0 +1,32 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def GateId = resultJson.gateid
+def ProjectId = resultJson.projectid
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST',/* requestBody: 
+  """{
+    	"data":
+	{
+		"repoType": "hosted",
+        "id": ${rid},
+        "name": ${repoName},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+	}
+        
+   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/select?gateId=${GateId}&projectId=${ProjectId}"
+}
+	def call(){
+def request = libraryResource 'sonarConnectorData.json'
+createRepo(request)
+}
