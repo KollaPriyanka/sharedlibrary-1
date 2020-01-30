1149b166878ce00d7c40fbd340f8c7f92759c732
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 18:28:06 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -12,17 +12,7 @@ httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON"
     
     httpMode: 'POST',/* requestBody: 
   """{
-    	"data":
-	{
-		"repoType": "hosted",
-        "id": ${rid},
-        "name": ${repoName},
-        "repoPolicy": "RELEASE",
-        "provider": "maven2",
-        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
-        "exposed": true,
-        "format": "maven2"
-	}
+    	
         
    }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"
 }
