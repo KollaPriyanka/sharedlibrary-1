b093053de45169783cd00f5af01c60b6ddd01f88
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 18:39:47 +0530

    Create sonar.groovy

---
+++ vars/sonar.groovy
@@ -0,0 +1,32 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def ProjectName = resultJson.pname
+def ProjectKey = resultJson.pkey
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST',/* requestBody: 
+  """{
+    	"data":
+	{
+		"repoType": "hosted",
+        "id": ${rid},
+        "name": ${repoName},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+	}
+        
+   }""",*? url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectName}&name=${ProjectKey}"
+}
+	def call(){
+def request = libraryResource 'sonarConnectorData.json'
+createRepo(request)
+}
