745be63a77a4ab5a237e637735e64a35ef5fb29d
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 19:01:13 +0530

    Create soanrQualityGate.groovy

---
+++ vars/soanrQualityGate.groovy
@@ -0,0 +1,31 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def QualityGateName = resultJson.qname
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST',/* requestBody: 
+  """{
+    	"data":
+	{
+		"repoType": "hosted",
+        "id": ${rid},
+        "name": ${repoName},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+	}
+        
+   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
+}
+	def call(){
+def request = libraryResource 'sonarConnectorData.json'
+createRepo(request)
+}
