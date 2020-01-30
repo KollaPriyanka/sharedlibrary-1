1a652db3eff55829d53c6b8432c8ae8c37cdd838
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 16:05:56 +0530

    Update sonarConnector.groovy

--- vars/sonarConnector.groovy
+++ vars/sonarConnector.groovy
@@ -13,7 +13,7 @@ def ProjectId = resultJson.projectid
 
 httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'POST', url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}", url: "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}", url: "http://3.16.33.107:9000/api/qualitygates/list", url: "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10", url: "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=critical_violations&op=GT&warning=5&error=10"', url: "http://3.16.33.107:9000/api/qualitygates/select?gateId=${GateId}&projectId=${ProjectId}"
+    httpMode: 'POST', url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}", "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}", "http://3.16.33.107:9000/api/qualitygates/list","http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=blocker_violations&op=GT&warning=5&error=10", "http://3.16.33.107:9000/api/qualitygates/create_condition?gateId=${GateId}&metric=critical_violations&op=GT&warning=5&error=10"',"http://3.16.33.107:9000/api/qualitygates/select?gateId=${GateId}&projectId=${ProjectId}"
 }
 	def call(){
 def request = libraryResource 'sonarConnectorData.json'
