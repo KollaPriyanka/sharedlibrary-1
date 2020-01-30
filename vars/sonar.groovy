cd8836896e81f461cda626d86084fe86ca436a93
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 18:45:31 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -6,6 +6,7 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parseText(data)
 def ProjectName = resultJson.pname
 def ProjectKey = resultJson.pkey
+def QualityGateName = resultJson.qname
 //def projUrl = resultJson.url
 
 httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
@@ -25,6 +26,24 @@ httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON"
 	}
         
    }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectName}&name=${ProjectKey}"
+	
+httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST',/* requestBody: 
+  """{
+    	"data":
+	{
+		"repoType": "hosted",
+        "id": ${rid},
+        "name": ${repoName},
+        "repoPolicy": "RELEASE",
+        "provider": "maven2",
+        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
+        "exposed": true,
+        "format": "maven2"
+	}
+        
+   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
 }
 	def call(){
 def request = libraryResource 'sonarConnectorData.json'
