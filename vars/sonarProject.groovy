fb200eff580f67a292cfaf08547aa205c7f8ebb6
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-01-30 18:55:41 +0530

    Update and rename sonar.groovy to sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -27,23 +27,6 @@ httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON"
         
    }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectName}&name=${ProjectKey}"
 	
-httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
-    
-    httpMode: 'POST',/* requestBody: 
-  """{
-    	"data":
-	{
-		"repoType": "hosted",
-        "id": ${rid},
-        "name": ${repoName},
-        "repoPolicy": "RELEASE",
-        "provider": "maven2",
-        "providerRole": "org.sonatype.nexus.proxy.repository.Repository",
-        "exposed": true,
-        "format": "maven2"
-	}
-        
-   }""",*/ url: "http://3.16.33.107:9000/api/qualitygates/create?name=${QualityGateName}"
 }
 	def call(){
 def request = libraryResource 'sonarConnectorData.json'
