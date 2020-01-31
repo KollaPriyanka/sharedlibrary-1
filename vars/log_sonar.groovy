046d9b5c280c215b6781c3e6c7bf1e23380d3766
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-31 15:19:10 +0530

    Update log_sonar.groovy

--- vars/log_sonar.groovy
+++ vars/log_sonar.groovy
@@ -1,1 +1,12 @@
+import groovy.json.JsonSlurper 
+@NonCPS
 
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'sonarConnectorData.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def rpkey = resultJson.pkey
+   sh " echo '${rid}' ${message} >>log.txt"
+}
