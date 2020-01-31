185e7957188a4b7bb89b149852742568ac970e1d
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-01-31 12:45:37 +0530

    Update sonarProject.groovy

--- vars/sonarProject.groovy
+++ vars/sonarProject.groovy
@@ -7,9 +7,10 @@ def resultJson = jsonSlurper.parseText(data)
 def ProjectKey = resultJson.pkey
 def ProjectName = resultJson.pname
 def QualityGateName = resultJson.qname
+def credentials = resultJson.cname
 //def projUrl = resultJson.url
 
-httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON", 
+httpRequest authentication: '${credentials}', contentType: "APPLICATION_JSON", 
     
     httpMode: 'POST',/* requestBody: 
   """{
@@ -25,7 +26,7 @@ httpRequest authentication: 'sonar_credentials', contentType: "APPLICATION_JSON"
         "format": "maven2"
 	}
         
-   }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectName}&name=${ProjectKey}"
+   }""",*/ url: "http://ec2-3-16-33-107.us-east-2.compute.amazonaws.com:9000/api/projects/create?key=${ProjectKey}&name=${ProjectName}"
 	
 }
 	def call(){
