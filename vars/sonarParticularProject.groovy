3ebbd7a1fe69471b683c10aeac8d0df97084fac2
SumaVarshitha <55625932+SumaVarshitha@users.noreply.github.com>
2020-02-04 18:36:45 +0530

    Update sonarParticularProject.groovy

--- vars/sonarParticularProject.groovy
+++ vars/sonarParticularProject.groovy
@@ -5,7 +5,7 @@ detailplan(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def colkey= resultJson.colkey
-
+def credentials = resultJson.cname
 httpRequest authentication: "${credentials}", contentType: "APPLICATION_JSON", 
     
     httpMode: 'GET', url: "http://3.16.33.107:9000/api/measures/component?metricKeys=ncloc,complexity,violations&component=${colkey}"
